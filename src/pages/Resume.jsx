555dd219983e0ce8c4024d6d86c9ee69002f1119
Ishneet Singh <ishneet100@gmail.com>
2023-07-12 14:25:42 -0600

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { resumeContext } from "../context";
+import ReactToPrint from "react-to-print";
+import { ArrowDown } from "react-feather";
 
 const Resume = () => {
   const { resume } = useContext(resumeContext);
+  const componentRef = useRef(null);
 
   return (
     <div>
@@ -11,8 +14,29 @@ const Resume = () => {
           {key}: {value}
         </div>
       ))}
+      <ReactToPrint
+        trigger={() => (
+          <button>
+            Download <ArrowDown />
+          </button>
+        )}
+        content={() => componentRef.current}
+      />
+      <div style={{ display: "none" }}>
+        <PrintContent ref={componentRef} resume={resume} />
+      </div>
     </div>
   );
 };
+
+const PrintContent = React.forwardRef(({ resume }, ref) => (
+  <div ref={ref}>
+    {Object.entries(resume).map(([key, value]) => (
+      <div key={key}>
+        {key}: {value}
+      </div>
+    ))}
+  </div>
+));
 
 export default Resume;
