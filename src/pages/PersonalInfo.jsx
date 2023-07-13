fbfc8b00dfee76503bf02dc16c9710a87636ce78
ishneeeeeet <ishneet100@gmail.com>
2023-07-12 21:30:06 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/pages/PersonalInfo.jsx

--- src/pages/PersonalInfo.jsx
+++ src/pages/PersonalInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import { resumeContext } from "../context";
