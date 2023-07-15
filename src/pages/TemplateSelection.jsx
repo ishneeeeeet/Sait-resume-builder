4057db9cc23205edc79d8ff137d656799741491b
ishneeeeeet <ishneet100@gmail.com>
2023-07-15 13:56:16 -0600

    modified:   src/pages/TemplateSelection.jsx 	new file:   src/templates.js

--- src/pages/TemplateSelection.jsx
+++ src/pages/TemplateSelection.jsx
@@ -1,36 +1,85 @@
-import React, { useContext} from "react";
+import React, { useContext, useState } from "react";
 import { resumeContext } from "../context";
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
+import { Template1, Template2, Template3 } from "../templates";
 
-const TemplateSelection = ({ templates, onSelectTemplate }) => {
+const TemplateSelection = () => {
   const { resume } = useContext(resumeContext);
-  const courseType = resume.course;
-  switch (courseType) {
-    case "IT":
-      console.log("It is IT");
-      return (
-        <>
-          <div>Select your resume</div>
-          <p>Course Type: IT</p>
-          <Link to="/personalinfo">
-            <Button text={"Next: Personal Info"} />
-          </Link>
-        </>
-      );
-    case "bakery":
-      console.log("It is bakery");
-      break;
-    default:
-      break;
-  }
+  const [selectedTemplate, setSelectedTemplate] = useState(null);
+
+  const handleTemplateSelect = (templateName) => {
+    setSelectedTemplate(templateName);
+    console.log(selectedTemplate);
+  };
+
+  const renderTemplatePreviews = () => {
+    return (
+      <div className="template-preview-container">
+        {resume.course === "IT services" && (
+          <div
+            className={`template-preview relative ${selectedTemplate === "Template1" ? "selected" : ""}`}
+            
+           
+          >
+            <Template1 />
+            {selectedTemplate === "Template1" && (
+              <button
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                onClick={() => handleTemplateSelect("Template1")}
+              >
+                Select
+              </button>
+            )}
+          </div>
+        )}
+        {resume.course === "Bakery" && (
+          <div
+            className={`template-preview relative ${selectedTemplate === "Template2" ? "selected" : ""}`}
+            onMouseEnter={() => handleTemplateSelect("Template2")}
+            onMouseLeave={() => handleTemplateSelect(null)}
+          >
+            <Template2 />
+            {selectedTemplate === "Template2" && (
+              <button
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                onClick={() => handleTemplateSelect("Template2")}
+              >
+                Select
+              </button>
+            )}
+          </div>
+        )}
+        {!resume.course && (
+          <div
+            className={`template-preview relative ${selectedTemplate === "Template3" ? "selected" : ""}`}
+            onMouseEnter={() => handleTemplateSelect("Template3")}
+            onMouseLeave={() => handleTemplateSelect(null)}
+          >
+            <Template3 />
+            {selectedTemplate === "Template3" && (
+              <button
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                onClick={() => handleTemplateSelect("Template3")}
+              >
+                Select
+              </button>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  };
+
   return (
     <>
       <div>Select your resume</div>
-      <Link to="/personalinfo">
-        <Button text={"Next: Personal Info"} />
-      </Link>
-      {courseType}
+      {renderTemplatePreviews()}
+      
+        <Link to="/personalinfo">
+          <Button text={"Next: Personal Info"} />
+        </Link>
+      
     </>
   );
 };
---
+++ src/templates.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+const Template1 = () => {
+  return (
+    <div className="bg-gray-100 p-8">
+      <h1 className="text-2xl font-bold mb-4">Resume</h1>
+      <div className="mb-8">
+        <h2 className="text-xl font-bold mb-2">Personal Information</h2>
+        <p className="text-gray-700">Name: John Doe</p>
+        <p className="text-gray-700">Email: john.doe@example.com</p>
+        <p className="text-gray-700">Phone: (123) 456-7890</p>
+      </div>
+      <div className="mb-8">
+        <h2 className="text-xl font-bold mb-2">Education</h2>
+        <p className="text-gray-700">Degree: Bachelor of Science</p>
+        <p className="text-gray-700">Major: Computer Science</p>
+        <p className="text-gray-700">University: Example University</p>
+        <p className="text-gray-700">Year: 2020</p>
+      </div>
+      <div>
+        <h2 className="text-xl font-bold mb-2">Experience</h2>
+        <p className="text-gray-700">Company: Example Company</p>
+        <p className="text-gray-700">Position: Software Engineer</p>
+        <p className="text-gray-700">Duration: 2018 - Present</p>
+        <p className="text-gray-700">Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+};
+
+const Template2 = () => {
+  return (
+    <div className="bg-blue-200 p-8">
+      <div className="text-center mb-8">
+        <h1 className="text-2xl font-bold">Resume</h1>
+        <p className="text-sm">John Doe</p>
+        <p className="text-sm">john.doe@example.com | (123) 456-7890</p>
+      </div>
+      <div className="mb-8">
+        <h2 className="text-lg font-bold mb-2">Education</h2>
+        <p className="text-gray-700">Degree: Bachelor of Science</p>
+        <p className="text-gray-700">Major: Computer Science</p>
+        <p className="text-gray-700">University: Example University</p>
+        <p className="text-gray-700">Year: 2020</p>
+      </div>
+      <div>
+        <h2 className="text-lg font-bold mb-2">Experience</h2>
+        <p className="text-gray-700">Company: Example Company</p>
+        <p className="text-gray-700">Position: Software Engineer</p>
+        <p className="text-gray-700">Duration: 2018 - Present</p>
+        <p className="text-gray-700">Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+};
+
+const Template3 = () => {
+  return (
+    <div className="bg-yellow-100 p-8">
+      <h1 className="text-2xl font-bold mb-4">Resume</h1>
+      <div className="mb-8">
+        <h2 className="text-xl font-bold mb-2">Personal Information</h2>
+        <p className="text-gray-700">Name: John Doe</p>
+        <p className="text-gray-700">Email: john.doe@example.com</p>
+        <p className="text-gray-700">Phone: (123) 456-7890</p>
+      </div>
+      <div className="mb-8">
+        <h2 className="text-xl font-bold mb-2">Education</h2>
+        <p className="text-gray-700">Degree: Bachelor of Science</p>
+        <p className="text-gray-700">Major: Computer Science</p>
+        <p className="text-gray-700">University: Example University</p>
+        <p className="text-gray-700">Year: 2020</p>
+      </div>
+      <div>
+        <h2 className="text-xl font-bold mb-2">Experience</h2>
+        <p className="text-gray-700">Company: Example Company</p>
+        <p className="text-gray-700">Position: Software Engineer</p>
+        <p className="text-gray-700">Duration: 2018 - Present</p>
+        <p className="text-gray-700">Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+};
+
+export { Template1, Template2, Template3 };
