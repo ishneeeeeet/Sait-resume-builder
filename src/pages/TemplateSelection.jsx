ee67d669ca5eb811afaa31ed49f4d884d090e412
Ishneet Singh <ishneet100@gmail.com>
2023-07-17 14:32:42 -0600

    modified:   src/pages/TemplateSelection.jsx

--- src/pages/TemplateSelection.jsx
+++ src/pages/TemplateSelection.jsx
@@ -1,27 +1,48 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { resumeContext } from "../context";
-import { Link } from "react-router-dom";
-import Button from "../components/Button";
 import TemplateOne from "../templates/TemplateOne";
 import TemplateTwo from "../templates/TemplateTwo";
 import TemplateThree from "../templates/TemplateThree";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 const TemplateSelection = () => {
   const { resume, setResume } = useContext(resumeContext);
 
+  const handleTemplateSelection = (templateName) => {
+    setResume((prevResume) => ({
+      ...prevResume,
+      template: templateName,
+    }));
+  };
+
   return (
     <>
-      <div className="flex flex-wrap gap-6">
+      <h1>Select Template</h1>
+      <div className="flex flex-col gap-6">
         <div className="w-1/3">
-          <TemplateOne />
+          <Button
+            text={"Template 1"}
+            onClick={() => handleTemplateSelection("Template 1")}
+          />
         </div>
         <div className="w-1/3">
-          <TemplateTwo />
+          <Button
+            text={"Template 2"}
+            onClick={() => handleTemplateSelection("Template 2")}
+          />
         </div>
         <div className="w-1/3">
-          <TemplateThree />
+          <Button
+            text={"Template 3"}
+            onClick={() => handleTemplateSelection("Template 3")}
+          />
         </div>
       </div>
+      <Link to="/personalinfo">
+        <Button text={"Next:"} />
+      </Link>
+      <p>Selected Template: {resume.template}</p>
     </>
   );
 };
